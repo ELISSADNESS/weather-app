049651aec6167d7053c1495158c956c16adaab65
Elizaveta <eliza85d@gmail.com>
2023-12-07 13:02:46 +0700

    07.12.23

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
 
+import '@mantine/carousel/styles.css';
 import { Carousel } from '@mantine/carousel';
 
-import '@mantine/carousel/styles.css';
 
-import styles from './app.module.css';
-
-const API = "https://api.open-meteo.com/v1/forecast?latitude=55.0415&longitude=82.9346&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto&start_date=2023-12-04&end_date=2023-12-06"
+const API = "https://api.open-meteo.com/v1/forecast?latitude=55.0415&longitude=82.9346&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto&start_date=2023-12-06&end_date=2023-12-08"
 
 const checkReponse = (res) => {
   return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
@@ -20,8 +18,14 @@ const sliderStyle = {
   includeGapInSize: false,
   styles: {
     slide: {
-      
-    }
+      // backgroundColor: '#ccecff',
+      // backgroundColor: 'white',
+      border: '3px solid #fff',
+      color: '#fff',
+      height: '50vh',
+      borderRadius: '20px',
+      padding: '2rem',
+    },
   }
 }
 
@@ -45,54 +49,32 @@ function App() {
   }, []);
 
   if (weather.data) {
-
     console.log(weather.data);
-
-    return (
-      <>
-        <h1>Weather App</h1>
-
-        {/* <Carousel slideSize="70%" height={200} slideGap="xl" controlsOffset="xs" controlSize={40} loop withIndicators >
-          <Carousel.Slide className={styles.slide}>
-            <p>Максимальная температура вчера: {weather.data.daily.temperature_2m_max[0]}</p>
-            <p>Минимальная температура вчера: {weather.data.daily.temperature_2m_min[0]}</p>
-          </Carousel.Slide>
-          <Carousel.Slide className={styles.slide}>
-            <h2>Температура сейчас: {weather.data.current.temperature_2m}</h2>
-            <p>Максимальная температура сегодня: {weather.data.daily.temperature_2m_max[1]}</p>
-            <p>Минимальная температура сегодня: {weather.data.daily.temperature_2m_min[1]}</p> 
-          </Carousel.Slide>
-          <Carousel.Slide className={styles.slide}>
-            <p>Максимальная температура завтра: {weather.data.daily.temperature_2m_max[2]}</p>
-            <p>Минимальная температура завтра: {weather.data.daily.temperature_2m_min[2]}</p>
-          </Carousel.Slide>
-        </Carousel> */}
-
-
-        {
-          weather.data &&
-          <Carousel {...sliderStyle}>
-            {
-              weather.data.daily.time.map((item, index) => {
-                <Carousel.Slide key={index}>
-                  <p>{item}</p>
-                  <p>{weather.data.daily.temperature_2m_max[index]}</p>
-                  <p>{weather.data.daily.temperature_2m_min[index]}</p>
-                </Carousel.Slide>
-              })
-            }
-          </Carousel>
-        }
-
-      </>
-    );
   }
 
-  // return (
-  //   <>
+  return (
+    <>
+      <h1 style={{padding: '15px', color: 'white'}}>Weather App</h1>
 
-  //   </>
-  // );
+      {
+        weather.data &&
+        <Carousel {...sliderStyle}>
+          {
+            weather.data.daily.time.map((item, index) => (
+              <Carousel.Slide key={index}>
+                <p style={{backgroundColor: 'white', borderRadius: '10px', padding: '10px', width: '110px', color: '#1d92db'}}>{item}</p>
+                
+                <p>{weather.data.current.temperature_2m}</p>
+                <p>максимальная температура: {weather.data.daily.temperature_2m_max[index]}</p>
+                <p>минимальная температура: {weather.data.daily.temperature_2m_min[index]}</p>
+              </Carousel.Slide>
+            ))
+          }
+        </Carousel>
+      }
+
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ root.render(
     < MantineProvider >
       <App />
     </MantineProvider>
-
   </React.StrictMode>
 );
 
